7e642c91b7242943b830c00f16a71f3df9e0ef66
fkurbonov <farhodjon.uz@gmail.com>
2020-08-11 12:03:32 +0500

    Dispatchers and Schedulers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,44 +2,36 @@ import kotlinx.coroutines.*
 import main.CoroutineContextProviderImpl
 import main.*
 import scopeProvider.CustomScope
+import java.util.concurrent.Executors
+
+// = = = Key points = = =
+//• important concepts in computing, when using execution algorithms, is scheduling and context switching.
+//• Scheduling takes care of resource management by coordinating threading and the lifecycle of processes.
+//• To communicate thread and process states in computing and task execution, the system uses context switching and dispatching.
+//• Context switching helps the system store thread and process state, so that it can switch between tasks which need execution.
+//• Dispatching handles which tasks get resources at which point in time.
+//• ContinuationInterceptors, which take care of the input/output of threading, and the main and background threads are provided through the Dispatchers class.
+//• Dispatchers can be confined and unconfined, where being confined or not relates to using a fixed threading system.
+//• There are four main dispatchers: Default, IO, Main and Unconfined.
+//• Using the Executors class you can create new thread pools to use for your
+//coroutine work.
 
 fun main() {
-  //Define your background process
-  val backgroundContextProvider = CoroutineContextProviderImpl(Dispatchers.Default).context()
-  //Create your fully controllable coroutine scope by providing context and exception handler
-  val mainScope = CustomScope(backgroundContextProvider, coroutineErrorHandler)
-  //Launch your async job in your scope and manage it
-  multipleAwaitExample(mainScope)
 
-  Thread.sleep(5000)
-//No need to send request to API so cancel Job
-  //mainScope.onStop()
+  //= = = Unconfined Dispatcher = = =
+  GlobalScope.launch(context = Dispatchers.Unconfined) {
+    println(Thread.currentThread().name) }
+  Thread.sleep(50)
 
+  //= = = stealing Executor = = =
+//  val executorDispatcher = Executors
+//      .newWorkStealingPool()
+//      .asCoroutineDispatcher()
+//
 
+//    GlobalScope.launch(context = executorDispatcher) { println(Thread.currentThread().name) }
+//    Thread.sleep(50)
 
-
-}
-
-fun multipleAwaitExample( mainScope: CoroutineScope) {
-  val userId = 992 // the ID of the user we want
-  val job = mainScope.launch {
-    val userDeferred = getUserByIdFromNetwork(userId, this)
-    val usersFromFileDeferred = readUsersFromFile("users.txt", this)
-
-    //todo: try to cause exception
-    //val usersFromFileDeferred = readUsersFromFile("user2s.txt", this)
-
-    if (isActive) {
-      println("Finding user")
-      val userStoredInFile = checkUserExists(
-          userDeferred.await(),
-          usersFromFileDeferred.await()
-      )
-
-      if (userStoredInFile) {
-        println("Found user in file!")
-      }
-    }
-  }
 
 }
+
