7d264a8294ff70eb593d622dadb63de3fb2f4c71
fkurbonov <farhodjon.uz@gmail.com>
2020-08-13 17:17:21 +0500

    Channels:Basic Example of channel. Understanding Send/Receive methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,60 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
 
-// = = = Key points = = =
-//Collection are eagerly evaluated; i.e., all items are operated upon completely before passing the result to the next operator.
+// = = = = = = Key points = = = = = =
+//  Channels provide the functionality for sending and receiving streams of values.
 
-//Sequence handles the collection of items in a lazy-evaluated manner; i.e., the items in it are not evaluated until you access them.
+//  Channel implements both SendChannel and ReceiveChannel interfaces; therefore, it could be used for sending and receiving streams of values.
 
-//Sequences are great at representing collection where the size isn’t known in advance, like reading lines from a file.
+//  A Channel can be closed. When that happens, you can’t send or receive an element from it.
 
-//asSequence() can be used to convert a list to a sequence.
+//  The send() method either adds the value to a channel or suspends the coroutine until there is space in the channel.
 
-//It is recommended to use simple Iterables in most of the cases, the benefit of using a sequence is only when there is a huge/infinite collection of elements with multiple operations.
+//  The receive() method returns a value from a channel if it is available, or it suspends the coroutine until some value is available otherwise.
 
-//Generators is a special kind of function that can return values and then can be resumed when they’re called again.
+//  The offer() method can be used as an alternative to send(). Unlike the send() method, offer() doesn’t suspend the coroutine, it returns false instead.
+// It returns true in case of a successful operation.
 
-//Using Coroutines with Sequence it is possible to implement Generators.
+//  poll() similarly to offer() doesn’t suspend the running, but returns null if a channel is empty.
 
-//SequenceScope is defined for yielding values of a Sequence or an Iterator using suspending functions or Coroutines.
+//  Java BlockingQueue has a similar to Kotlin Channel behavior, the main difference is that the current thread gets
+// blocked if the operation of inserting or retrieving is unavailable at the moment.
 
-//SequenceScope provides yield() and yieldAll() suspending functions to enable Generator function behavior.
 fun main()  {
+  //1
+  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes", "Strawberry")
+  // 2 Init Producer
+  val kotlinChannel = Channel<String>()
+  runBlocking() {
+    //3 Send data into Channel
+    GlobalScope.launch {
+      for (fruit in fruitArray) {
+        //4
+        kotlinChannel.send(fruit)
 
-// = = = Key points = = =
+        //5
+        if (fruit == "Pear") {
+          //6
+          kotlinChannel.close()
+        }
+      }
+    }
+    //7 Consumer via If operator <- use this operator because it understands Iterator and sequence under the hood and does it in proficient way
+//    for (fruit in kotlinChannel) {
+//      println(fruit)
+//    }
+    //7 Consumer via while operator
+    while (!kotlinChannel.isClosedForReceive) {
+      val value = kotlinChannel.receive()
+      println(value)
+    }
 
-// = = = = =  YieldAll - Passing Sequence instead of Iterator= = = = = =
-
-  // 1
-  val sequence = sequenceExample().take(100)
-
-  // 2
-  sequence.forEach {
-    print("$it ")
+    //8
+    println("Done")
   }
 }
-// 3
-fun sequenceExample() = sequence {
-  // 4
-  yieldAll( generateSequence( 2 ) { it*2 } )
-}
+
 
 
 
