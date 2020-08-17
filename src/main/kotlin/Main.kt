8a39c8d4df7d81d8c76ada1c41bced94c1beefbd
fkurbonov <farhodjon.uz@gmail.com>
2020-08-17 10:52:43 +0500

    Broadcast Channels - Broadcast Channel Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,64 +1,57 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 // = = = = = = Key points = = = = = =
-//  Channels provide the functionality for sending and receiving streams of values.
-
-//  Channel implements both SendChannel and ReceiveChannel interfaces; therefore, it could be used for sending and receiving streams of values.
-
-//  A Channel can be closed. When that happens, you can’t send or receive an element from it.
-
-//  The send() method either adds the value to a channel or suspends the coroutine until there is space in the channel.
-
-//  The receive() method returns a value from a channel if it is available, or it suspends the coroutine until some value is available otherwise.
-
-//  The offer() method can be used as an alternative to send(). Unlike the send() method, offer() doesn’t suspend the coroutine, it returns false instead.
-// It returns true in case of a successful operation.
-
-//  poll() similarly to offer() doesn’t suspend the running, but returns null if a channel is empty.
-
-//  Java BlockingQueue has a similar to Kotlin Channel behavior, the main difference is that the current thread gets
-// blocked if the operation of inserting or retrieving is unavailable at the moment.
+//With channels, if you have many receivers waiting to receive items from the channel, the emitted item will be consumed by the first receiver and all other receivers will not get the item individually.
+//BroadcastChannel enables many subscribed receivers to consume all items sent in the channel.
+//ConflatedBroadcastChannel enables many subscribed receivers to consume the most recently sent item provided the receiver consumes items slower.
+//Subject from RxJava is the dual of the BroadcastChannel in behavior.
+//BehaviorSubject from RxJava is the dual of ConflatedBroadcastChannel in behavior
 //=====================================================================================================================
 
-
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
 fun main() {
   // 1 A string array of fruit names is created, named fruitArray.
-  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes",
-      "Strawberry")
-  // 2
-  val kotlinChannel = Channel<String>()
+  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes", "Strawberry")
+  val kotlinChannel = BroadcastChannel<String>(3)
 
-  // 3
   runBlocking {
-
-    // 4 Producer
-    GlobalScope.launch {
-      // Send data in channel
-      kotlinChannel.send(fruitArray[0])
+    //2 Feed channel
+    kotlinChannel.apply {
+      send(fruitArray[0])
+      send(fruitArray[1])
+      send(fruitArray[2])
     }
+    //3 “Start consuming items from the channel. Consumer 1
+    GlobalScope.launch {
+      //4 “Here, a subscription is opened on the kotlinChannel using the openSubscription() function — i.e.,
+      //start listening to values being sent in the kotlinChannel.
+      kotlinChannel.consumeEach{ value  ->
+        //5 “Iterate over all the values in the channel and print them out
+          println("Consumer 1: $value")
 
-    // 5 Consumers
+      }
+      //6 When finished iterating over the values in the channel, the subscription is closed —
+      //i.e., stop listening to values being sent in the kotlinChannel.
+    }
+    // Consumer 2
     GlobalScope.launch {
       kotlinChannel.consumeEach { value ->
-        println("Consumer 1: $value")
+          println("Consumer 2: $value")
       }
     }
-    GlobalScope.launch {
-      kotlinChannel.consumeEach { value ->
-        println("Consumer 2: $value")
-      }
+    //7 Now that the subscription has been obtained on the kotlinChannel, send two more values in the kotlinChannel.
+    kotlinChannel.apply {
+      send(fruitArray[3])
+      send(fruitArray[4])
     }
-
-    // 6
+    //8
     println("Press a key to exit...")
     readLine()
 
-    // 7
+    //9
     kotlinChannel.close()
   }
 
