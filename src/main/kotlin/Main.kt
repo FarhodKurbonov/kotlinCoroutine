5f65fb409284bad0c819fcc874ce124cb4b597cd
fkurbonov <farhodjon.uz@gmail.com>
2020-08-17 11:42:55 +0500

    Broadcast Channels - PRODUCER-CONSUMER model to broadcast message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,38 @@
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 // = = = = = = Key points = = = = = =
-//ConflatedBroadcastChannel only emits the most recently sent item while the older items are lost.
-//Also, any future subscribers to this channel will receive the item that was most recently emitted.
+//Multi processing communication using PRODUCER-CONSUMER approach to broadcast message
 //=====================================================================================================================
 
 
-@ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 fun main() {
-  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes", "Strawberry")
-  //1
-  val kotlinChannel = ConflatedBroadcastChannel<String>()
-  runBlocking {
-    kotlinChannel.apply {
-      send(fruitArray[0])
-      send(fruitArray[1])
-      send(fruitArray[2])
-    }
-    // 3
-    GlobalScope.launch {
-      kotlinChannel.consumeEach { value ->
-        println("Consumer 1: $value")
-      }
-    }
-    GlobalScope.launch {
-      kotlinChannel.consumeEach { value ->
-        println("Consumer 2: $value")
-      }
-    }
+  val receivedChannel = GlobalScope.produce(capacity = 10) {
+    while (isActive) {
+      val number = Random.nextInt(0, 20)
+      send(number)
+      println("$number send")
 
-    // 4
-    kotlinChannel.apply {
-      send(fruitArray[3])
-      send(fruitArray[4])
     }
-
-    // 5
-    println("Press a key to exit...")
-    readLine()
-
-    // 6
-    kotlinChannel.close()
   }
+
+  GlobalScope.launch {
+    receivedChannel.consumeEach { println("Consumer 1: $it received") }
+  }
+
+  GlobalScope.launch {
+    receivedChannel.consumeEach { println("Consumer 2: $it received") }
+  }
+
+  Thread.sleep(30L)
 }
+
 
 
 
