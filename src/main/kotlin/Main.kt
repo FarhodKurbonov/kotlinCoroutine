63e94a945d83b939e6650444760df8f2e667a836
fkurbonov <farhodjon.uz@gmail.com>
2020-08-17 11:05:56 +0500

    Broadcast Channels - ConflatedBroadcastChannel example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,60 +1,51 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.channels.consumeEach
 
 // = = = = = = Key points = = = = = =
-//With channels, if you have many receivers waiting to receive items from the channel, the emitted item will be consumed by the first receiver and all other receivers will not get the item individually.
-//BroadcastChannel enables many subscribed receivers to consume all items sent in the channel.
-//ConflatedBroadcastChannel enables many subscribed receivers to consume the most recently sent item provided the receiver consumes items slower.
-//Subject from RxJava is the dual of the BroadcastChannel in behavior.
-//BehaviorSubject from RxJava is the dual of ConflatedBroadcastChannel in behavior
+//ConflatedBroadcastChannel only emits the most recently sent item while the older items are lost.
+//Also, any future subscribers to this channel will receive the item that was most recently emitted.
 //=====================================================================================================================
 
-@ExperimentalCoroutinesApi
-@ObsoleteCoroutinesApi
-fun main() {
-  // 1 A string array of fruit names is created, named fruitArray.
-  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes", "Strawberry")
-  val kotlinChannel = BroadcastChannel<String>(3)
 
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
+fun main() {
+  val fruitArray = arrayOf("Apple", "Banana", "Pear", "Grapes", "Strawberry")
+  //1
+  val kotlinChannel = ConflatedBroadcastChannel<String>()
   runBlocking {
-    //2 Feed channel
     kotlinChannel.apply {
       send(fruitArray[0])
       send(fruitArray[1])
       send(fruitArray[2])
     }
-    //3 “Start consuming items from the channel. Consumer 1
-    GlobalScope.launch {
-      //4 “Here, a subscription is opened on the kotlinChannel using the openSubscription() function — i.e.,
-      //start listening to values being sent in the kotlinChannel.
-      kotlinChannel.consumeEach{ value  ->
-        //5 “Iterate over all the values in the channel and print them out
-          println("Consumer 1: $value")
-
-      }
-      //6 When finished iterating over the values in the channel, the subscription is closed —
-      //i.e., stop listening to values being sent in the kotlinChannel.
-    }
-    // Consumer 2
+    // 3
     GlobalScope.launch {
       kotlinChannel.consumeEach { value ->
-          println("Consumer 2: $value")
+        println("Consumer 1: $value")
       }
     }
-    //7 Now that the subscription has been obtained on the kotlinChannel, send two more values in the kotlinChannel.
+    GlobalScope.launch {
+      kotlinChannel.consumeEach { value ->
+        println("Consumer 2: $value")
+      }
+    }
+
+    // 4
     kotlinChannel.apply {
       send(fruitArray[3])
       send(fruitArray[4])
     }
-    //8
+
+    // 5
     println("Press a key to exit...")
     readLine()
 
-    //9
+    // 6
     kotlinChannel.close()
   }
-
 }
 
 
